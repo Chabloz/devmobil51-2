3f5a6d9798d6cbe3f122721be04de24516cfa4b4
Chabloz Nicolas (SEC-GC) <nicolas.chabloz@etat.ge.ch>
2024-09-23 10:03:53 +0200

    Basic animation loop

---
+++ src/class/Circle.js
@@ -0,0 +1,36 @@
+import { TAU } from "../utils/math.js";
+
+export default class Circle {
+
+  constructor({
+    radius = 100,
+    color = "tomato",
+    x = 0,
+    y = 0,
+    speed = 100,
+    dir = 0,
+  } = {}) {
+    this.radius = radius;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.dir = dir;
+  }
+
+  move(dt) {
+      const dx = Math.cos(this.dir) * this.speed * dt / 1000;
+      const dy = Math.sin(this.dir) * this.speed * dt / 1000;
+      this.x += dx;
+      this.y += dy;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, TAU);
+    ctx.fill();
+    ctx.closePath();
+  }
+
+}
---
+++ src/main.js
@@ -0,0 +1,36 @@
+import Circle from "./class/Circle";
+import { TAU } from "./utils/math";
+
+const ctx = document.querySelector('canvas').getContext('2d');
+
+// set canvas size to match the parent container
+ctx.canvas.width = ctx.canvas.clientWidth;
+ctx.canvas.height = ctx.canvas.clientHeight;
+
+console.log(ctx.canvas.width, ctx.canvas.height);
+
+const circles = [];
+for (let i = 0; i < 10; i++) {
+  circles.push(new Circle({
+    x: Math.random() * ctx.canvas.width,
+    y: Math.random() * ctx.canvas.height,
+    speed: Math.random() * 100,
+    dir: Math.random() * TAU,
+    color: `hsl(${Math.random() * 360}, 100%, 50%)`,
+  }));
+}
+
+// raf loop
+function tick(timestamp) {
+  const dt = timestamp - lastTick;
+  lastTick = timestamp;
+  ctx.canvas.width = ctx.canvas.clientWidth;
+  ctx.canvas.height = ctx.canvas.clientHeight;
+  for (const circle of circles) {
+    circle.move(dt);
+    circle.draw(ctx);
+  }
+  requestAnimationFrame(tick);
+}
+let lastTick = 0;
+requestAnimationFrame(tick);
